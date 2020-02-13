60418b584a8a0845f9f11572e1f689ee13d29251
DaniloVitor2552 <danilovitor739@gmail.com>
2020-02-13 16:56:32 -0300

    delete nas series

--- src/server.js
+++ src/server.js
@@ -3,6 +3,15 @@ const app = express()
 
 app.use(express.json())
 
+let alowCrossDomain = (req,res,next)=>{
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE')
+    res.header('Access-Control-Allow-Headers', '*')
+    next()
+}
+
+app.use(alowCrossDomain)
+
 const auth = require('./routes/authRoutes')
 const series = require('./routes/seriesRoutes')
 
